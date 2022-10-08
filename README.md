6875a7188b80ba81c34947928ab4f7b0ea8a6f5b
JaneMusimbi <janemusimbij@gmail.com>
2022-10-08 12:06:20 +0300

    cheanges to Dev

---
+++ README.md
@@ -0,0 +1,9 @@
+**PROJECT NAME:SOLACE**
+
+**ABOUT THE PROJECT**
+
+Our project is in form of a mobile application that aims to assist Gen Z in Kenya manage anxiety disorder with resources such as guided meditation, breathing exercies and other envidence based methods in order to improve their quality of life.
+
+**TARGET USERS**
+
+Generation Z in Kenya currently
---
+++ app/src/main/java/com/yasmin/solace_application/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.yasmin.solace_application
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
